306306a50cb729c9f90f442ee71ef5bb404f5956
unknown <sconnolly@DevSConnolly.vistaprint.net>
2013-03-04 09:44:42 -0500

    Adding new top level directory, missed in previous commit.

---
+++ YahooFantasyFootballTools/YahooFantasyFootballTools/Models/LeagueModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YahooFantasyFootballTools.Models
+{
+    public class LeagueModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class LeagueModelList : List<LeagueModel>
+    {
+
+    }
+}
