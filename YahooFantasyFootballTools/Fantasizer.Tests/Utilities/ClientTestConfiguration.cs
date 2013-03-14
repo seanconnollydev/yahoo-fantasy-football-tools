7db82214f038019d1731a9df62130287534264f8
Sean Connolly <sconnolly17@gmail.com>
2013-03-13 21:09:20 -0400

    Renaming folders to reflect new client library name, Fantasizer.

---
+++ YahooFantasyFootballTools/Fantasizer.Tests/Utilities/ClientTestConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fantasizer.Tests.Utilities
+{
+    internal class ClientTestConfiguration
+    {
+        public const string USER_ACCESS_TOKEN = "A=a2ZXz0HimgOvVYlGVBSMg4jQNBO_ug31bH7gV8Ubabnp7mi27Rei6.gUIMPd0w8.GNbzl1kiE_6wdz4MdtFz1aFBY2Ja8ZA501qQ5CHpUJTbGikSQ9bQ51PINDfziVMBsbhQ1zhv3qWZCxA5kKK0uQl6ucStyRIxs7UhvPZfxYgiBYNQUgEMy0W8sOYkO3w5OzKPiYKpH.X249bAtDD9DsfNQNTV9smcrvTQmszbLsv1WlUFxtJxJVsdqrimeblW.4dI7wttjPwqZdqgqDyKxaQu_2eoQ0vxlMDXHZxS8CoVMJCQfJMiNMhJzChAn3_9F7jMwI9YhxackgyI6qYjQKwbXVJrBAycumj0B8Hqf85ZrNf.LksrGL.SGIl.tlM3jKjwsgp0W03X557kkdzPspIVDHs1Lo0uBiLSXbp8rzonals1XD59f6GLkZu64oWdKN7COgnRSKIJNvtMvCCdA9sz_FyynIclgQGlV4EtcWy0Tviky0JlAMkJeP0bX0YVsFZoTSNG763tn0eODENuE8xJG4a0bm_jVlitUKXMQVIGhCcxi_rgMnXBT4dEQQa92AsHABENqQ.StN2A7MJ2RLGr8SjRYmgV_N.IskT87LOGf5kEbHLF6D4BRI0mD2zoiozfZQl0GWvLHv6cljxoNg6mGhwgr0_y_BPygt3spMH56JUvjsmbmJVTlO316vEUW9fS9OIcA.CfssOTiNbShtZyOg_3vCilDp73Q9DqZ042cEaONX8QXos52Bm3Ur1TrLOnsOkKA.sROQZFEg--";
+        public const string USER_ACCESS_TOKEN_SECRET = "b4953dde9ae75746fd9a31a11dafe9f055e5bba7";
+
+        public const string CONSUMER_KEY = "dj0yJmk9OVJXSFBMcVZyRldJJmQ9WVdrOU9XaGhiblJ5TnpJbWNHbzlNVGd3TkRRd05qRTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD03MA--";
+        public const string CONSUMER_SECRET = "266f2c9d42e794b46aab16ce6c894986e008f3fa";
+        
+        public const string DEFAULT_LEAGUE_KEY = "273.l.86177";
+        public const string DEFAULT_TEAM_KEY = "273.l.86177.t.4"; // Wookie of the Year - 2012
+    }
+}
