e412285fd92a39141d3d55b83bbaeba91fabc7e9
Sean Connolly <sconnolly17@gmail.com>
2013-03-13 21:44:29 -0400

    Testing AppHarbor (again) to see if the problem is with my configuration lookup code #21.

--- YahooFantasyFootballTools/YahooFantasyFootballTools/Configuration.cs
+++ YahooFantasyFootballTools/YahooFantasyFootballTools/Configuration.cs
@@ -10,12 +10,14 @@ namespace YahooFantasyFootballTools
     {
         public static string ConsumerKey
         {
-            get { return Get<string>("YahooConsumerKey"); }
+            get { return "dj0yJmk9OVJXSFBMcVZyRldJJmQ9WVdrOU9XaGhiblJ5TnpJbWNHbzlNVGd3TkRRd05qRTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD03MA--"; }
+            //get { return Get<string>("YahooConsumerKey"); }
         }
 
         public static string ConsumerSecret
         {
-            get { return Get<string>("YahooConsumerSecret"); }
+            //get { return Get<string>("YahooConsumerSecret"); }
+            get { return "266f2c9d42e794b46aab16ce6c894986e008f3fa"; }
         }
 
         private static T Get<T>(string key)
