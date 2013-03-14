51446ff799ce159a58b977957af2adf48acd38d6
Sean Connolly <sconnolly17@gmail.com>
2013-03-13 22:11:40 -0400

    Adding missing ApHarborUtil file #21.

---
+++ YahooFantasyFootballTools/Fantasizer.Tests/Utilities/AppHarborUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fantasizer.Tests.Utilities
+{
+    public static class AppHarborUtil
+    {
+        public static void CheckSecrets()
+        {
+            if (string.IsNullOrEmpty(ClientTestConfiguration.ConsumerKey) ||
+                string.IsNullOrEmpty(ClientTestConfiguration.ConsumerSecret))
+            {
+                Assert.Inconclusive("Consumer key or secret could not be found, probably because this test is running on AppHarbor.");
+            } 
+        }
+    }
+}
